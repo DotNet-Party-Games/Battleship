87e7dbab251ded31be9ad5f82f7362ae94742c06
ArgusSanguine <argusandaurus@gmail.com>
2021-07-30 14:23:03 -0500

    Added ScoreDL

---
+++ BattleshipDL/IScoreDL.cs
@@ -0,0 +1,42 @@
+﻿using BattleshipModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipDL
+{
+    /// <summary>
+    /// Handle data logic for Score objects.
+    /// </summary>
+    public interface IScoreDL
+    {
+        /// <summary>
+        /// Gets a score from the database based on its Id
+        /// </summary>
+        /// <param name="p_sId">The ScoreId of the desired score</param>
+        /// <returns>Returns the score with matching ScoreId</returns>
+        Task<Score> GetScore(int p_sId);
+
+        /// <summary>
+        /// Adds a score to the database
+        /// </summary>
+        /// <param name="p_score">The score object to be added</param>
+        /// <returns>Returns the score object passed in as a parameter</returns>
+        Task<Score> AddScore(Score p_score);
+
+        /// <summary>
+        /// Gets all scores from the database
+        /// </summary>
+        /// <returns>Returns the scores as a List</returns>
+        Task<List<Score>> GetAllScores();
+
+        /// <summary>
+        /// Updates a score in the database
+        /// </summary>
+        /// <param name="p_score">The score to be updated, including changes</param>
+        /// <returns>Returns the score object passed in as a parameter</returns>
+        Task<Score> UpdateScore(Score p_score);
+    }
+}
---
+++ BattleshipDL/ScoreDL.cs
@@ -0,0 +1,44 @@
+﻿using BattleshipModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipDL
+{
+    public class ScoreDL : IScoreDL
+    {
+        private BattleshipDbContext _context;
+
+        public ScoreDL(BattleshipDbContext p_context)
+        {
+            _context = p_context;
+        }
+
+        public async Task<Score> AddScore(Score p_score)
+        {
+            _context.Scores.Add(p_score);
+            await _context.SaveChangesAsync();
+            return p_score;
+        }
+
+        public async Task<List<Score>> GetAllScores()
+        {
+            return await _context.Scores.Select(score => score).ToListAsync();
+        }
+
+        public async Task<Score> GetScore(int p_sId)
+        {
+            return await _context.Scores.FindAsync(p_sId);
+        }
+
+        public async Task<Score> UpdateScore(Score p_score)
+        {
+            _context.Scores.Update(p_score);
+            await _context.SaveChangesAsync();
+            return p_score;
+        }
+    }
+}
