c46a82305878d7cd509f35de35262b53c1cfca37
ArgusSanguine <argusandaurus@gmail.com>
2021-07-30 14:38:51 -0500

    Added StatisticDL

---
+++ BattleshipDL/IStatisticDL.cs
@@ -0,0 +1,42 @@
+﻿using BattleshipModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipDL
+{
+    /// <summary>
+    /// Handles data logic for Statistic objects
+    /// </summary>
+    public interface IStatisticsDL
+    {
+        /// <summary>
+        /// Gets a statistic based on its id
+        /// </summary>
+        /// <param name="p_sId">The StatId of the statistic to get</param>
+        /// <returns>Returns the statistic with a matching StatId</returns>
+        Task<Statistic> GetStatistic(int p_sId);
+
+        /// <summary>
+        /// Adds a new statistic to the database
+        /// </summary>
+        /// <param name="p_stat">The statistic object to be added</param>
+        /// <returns>Returns the statistic object passed in as a parameter</returns>
+        Task<Statistic> AddStatistic(Statistic p_stat);
+
+        /// <summary>
+        /// Returns all statistics in the database
+        /// </summary>
+        /// <returns>Returns the statistics in List form</returns>
+        Task<List<Statistic>> GetAllStatistics();
+
+        /// <summary>
+        /// Updates a statistic in the database
+        /// </summary>
+        /// <param name="p_stat">The statistic to be updates, including changes</param>
+        /// <returns>Returns the statistic object passed in as a parameter</returns>
+        Task<Statistic> UpdateStatistic(Statistic p_stat);
+    }
+}
---
+++ BattleshipDL/StatisticDL.cs
@@ -0,0 +1,44 @@
+﻿using BattleshipModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipDL
+{
+    public class StatisticDL : IStatisticDL
+    {
+        private BattleshipDbContext _context;
+
+        public StatisticDL(BattleshipDbContext p_context)
+        {
+            _context = p_context;
+        }
+
+        public async Task<Statistic> AddStatistic(Statistic p_stat)
+        {
+            _context.Statistics.Add(p_stat);
+            await _context.SaveChangesAsync();
+            return p_stat;
+        }
+
+        public async Task<List<Statistic>> GetAllStatistics()
+        {
+            return await _context.Statistics.Select(stat => stat).ToListAsync();
+        }
+
+        public async Task<Statistic> GetStatistic(int p_sId)
+        {
+            return await _context.Statistics.FindAsync(p_sId);
+        }
+
+        public async Task<Statistic> UpdateStatistic(Statistic p_stat)
+        {
+            _context.Statistics.Update(p_stat);
+            await _context.SaveChangesAsync();
+            return p_stat;
+        }
+    }
+}
