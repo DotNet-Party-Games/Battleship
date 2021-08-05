e2e79d90aad672857073844cd7f784e20153e467
WingHLin <winghlin1998@gmail.com>
2021-08-05 15:39:30 -0600

    Added CORS to WebAPI

--- BattleshipWebAPI/Startup.cs
+++ BattleshipWebAPI/Startup.cs
@@ -34,6 +34,17 @@ namespace BattleshipWebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BattleshipWebAPI", Version = "v1" });
             });
+
+            services.AddCors(
+                (builder) => {
+                    builder.AddDefaultPolicy((policy) =>
+                    {
+                        policy.WithOrigins("http://127.0.0.1:4200") //This is where you state the address that you want to trust
+                            .AllowAnyHeader() //Allows any header
+                            .AllowAnyMethod(); //Allows any http verb method
+                    });
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
