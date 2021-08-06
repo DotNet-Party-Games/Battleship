72f09d0d7a9658eb997d67bc2bd7188de1449c51
jrm246 <jrm246@att.net>
2021-08-06 17:41:24 -0400

    Added the controller for the Gameboard

---
+++ BattleshipWebAPI/Controllers/GameBoardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BattleshipModels;
+
+namespace BattleshipWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameBoardController : Controller
+    {
+        private GameBoard _gameBoard;
+
+        public GameBoardController()
+        {
+            _gameBoard = new GameBoard(10);
+        }
+
+        [HttpGet("get")]
+        public IActionResult GetGameBoard()
+        {
+            return Ok(_gameBoard);
+        }
+
+        [HttpPut]
+        public IActionResult PlaceShip(int shipId, )
+    }
+}
