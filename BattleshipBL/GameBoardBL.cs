207e10013d7a710d6f834f179b6ac2b67877dade
jrm246 <jrm246@att.net>
2021-08-06 17:41:46 -0400

    Added Business logic for the GameBoard

---
+++ BattleshipBL/GameBoardBL.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleshipModels;
+
+namespace BattleshipBL
+{
+    class GameBoardBL : IGameBoardBL
+    {
+        private GameBoard _gameBoard;
+        public GameBoardBL()
+        {
+            _gameBoard = new GameBoard(10);
+        }
+        public void SetUp(int User1Id, int User2Id)
+        {
+            // ToDo: Implement the users being the actual users
+            _gameBoard.User1 = new User() { UserId = User1Id };
+            _gameBoard.User2 = new User() { UserId = User2Id };
+        }
+        public bool Attack(int UserId, int x, int y, int z)
+        {
+            Position position = new Position(x, y, z);
+            if (_gameBoard.User1.UserId == UserId)
+            {
+                _gameBoard.User1Attacks(position);
+            }
+            else
+            {
+                _gameBoard.User2Attacks(position);
+            }
+            return _gameBoard.IsWinner() != null;
+        }
+
+        public void DeployShips(int UserId)
+        {
+            if (_gameBoard.User1.UserId == UserId)
+            {
+                _gameBoard.User1Navy.DeployShips();
+            }
+            else
+            {
+                _gameBoard.User2Navy.DeployShips();
+            }
+        }
+
+        public void PlaceShip(int UserId, int shipId, int x, int y, int z, bool horizontal)
+        {
+            Position position = new Position(x, y, z);
+            Orientation orientation = Orientation.Vertical;
+            if (horizontal)
+            {
+                orientation = Orientation.Horizontal;
+            }
+            if (_gameBoard.User1.UserId == UserId)
+            {
+                _gameBoard.User1Navy.PlaceShip(_gameBoard.User1Navy.Ships[shipId], position, orientation);
+            }
+            else
+            {
+                _gameBoard.User2Navy.PlaceShip(_gameBoard.User2Navy.Ships[shipId], position, orientation);
+            }
+        }
+    }
+}
---
+++ BattleshipBL/IGameBoardBL.cs
@@ -0,0 +1,45 @@
+﻿using BattleshipModels;
+
+namespace BattleshipBL
+{
+    /// <summary>
+    /// Handles the logic for running the game
+    /// </summary>
+    public interface IGameBoardBL
+    {
+        /// <summary>
+        /// Sets up the users playing the game
+        /// </summary>
+        /// <param name="User1Id">The Id of the first user</param>
+        /// <param name="User2Id">The Id of the second user</param>
+        public void SetUp(int User1Id, int User2Id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <param name="shipId"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="horizontal"></param>
+        public void PlaceShip(int UserId, int shipId, int x, int y, int z, bool horizontal);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserId"></param>
+        public void DeployShips(int UserId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        public bool Attack(int UserId, int x, int y, int z);
+
+    }
+}
