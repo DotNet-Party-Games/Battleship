cfc493dd34de56e8bfe99393dd6ca8ce5e29dad4
jrm246 <jrm246@att.net>
2021-08-10 11:23:14 -0400

    Added interfaces for the GameBoardBL and the GameRooms

--- BattleshipBL/IGameBoardBL.cs
+++ BattleshipBL/IGameBoardBL.cs
@@ -1,12 +1,37 @@
 ﻿using BattleshipModels;
+using System.Collections.Generic;
 
 namespace BattleshipBL
 {
+    /// <summary>
+    /// Holds a dictionary of IGameBoardBL
+    /// </summary>
+    public interface IGameRooms
+    {
+        /// <summary>
+        /// The Dictionary of IGameBoardBL
+        /// </summary>
+        public Dictionary<int, IGameBoardBL> GameBoards { get; set; }
+
+        /// <summary>
+        /// Gets the appropriate GameRoom from the Dictionary. If the roomId does not exist it will create a new entry
+        /// </summary>
+        /// <param name="roomId">The Id of the GameRoom</param>
+        /// <returns>The GameBoardBL that corresponds to the roomId. If no entry matches the roomId creates a new entry with the roomId and 
+        /// returns a new GameBoard</returns>
+        public IGameBoardBL GetGameRoom(int roomId);
+    }
+
     /// <summary>
     /// Handles the logic for running the game
     /// </summary>
     public interface IGameBoardBL
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public GameBoard GameBoard { get; set; }
+
         /// <summary>
         /// Sets up the users playing the game
         /// </summary>
