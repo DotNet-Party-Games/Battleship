db4a5d099acbfae1d9aa45c630de3e690e0ebbf6
jrm246 <jrm246@att.net>
2021-08-12 12:49:21 -0400

    Added graphics

--- BattleshipUI/src/app/game-board/game-board.component.ts
+++ BattleshipUI/src/app/game-board/game-board.component.ts
@@ -11,6 +11,7 @@ export class GameBoardComponent implements OnInit {
 
   width: number[];
   height: number[];
+  roomNumber: number;
   ocean: string[][][] = new Array(10);
   enemyOcean: string[][][] = new Array(10);
   selected: number[] = new Array(2);
@@ -19,12 +20,16 @@ export class GameBoardComponent implements OnInit {
   constructor(private GameApi: BattleshipAPIService) {
     this.width = new Array(10);
     this.height = new Array(10);
+    this.roomNumber = 0;
 
     for (let i = 0; i < 10; i++) {
       this.ocean[i] = new Array(10);
+      this.enemyOcean[i] = new Array(10);
       for (let j = 0; j < 10; j++) {
         this.ocean[i][j] = new Array(2);
+        this.enemyOcean[i][j] = new Array(2);
         this.ocean[i][j][0] = "water";
+        this.enemyOcean[i][j][0] = "water";
       }
     }
     this.selected[0] = 0;
@@ -33,32 +38,155 @@ export class GameBoardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.GetGameBoard(1);
+    this.GetGameBoard(this.roomNumber);
   }
 
   GetGameBoard(roomId: number) {
     this.GameApi.GetGameBoard(roomId).subscribe(
       (response) => {
         this.GameBoard[0] = response;
-        this.InterpretOcean(this.GameBoard[0].user1Navy.ocean);
-        this.enemyOcean = this.GameBoard[0].user1Navy.enemyOcean;
-        console.log(this.GameBoard[0]);
-        console.log(response["user1Navy"])
+        this.InterpretOcean(this.GameBoard[0].user1Navy.ocean, this.ocean);
+        this.InterpretOcean(this.GameBoard[0].user1Navy.enemyOcean, this.enemyOcean);
       }
     );
-    console.log(this.GameBoard[0]);
   }
 
+  Attack(x: number, y: number, z: number) {
+    this.GameApi.Attack(this.roomNumber, 1, x, y, z).subscribe(
+      (response) => {
+        this.GameBoard[0] = response;
+        this.InterpretOcean(this.GameBoard[0].user1Navy.ocean, this.ocean);
+        this.InterpretOcean(this.GameBoard[0].user1Navy.enemyOcean, this.enemyOcean);
+      }
+    )
+  }
 
-  InterpretOcean(item: number[][][]) {
+  InterpretOcean(item: number[][][], baseOcean: string[][][]) {
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
         switch (item[i][j][0]) {
+          case 0:
+            baseOcean[i][j][0] = "water";
+            break;
+          case 1:
+            baseOcean[i][j][0] = "hit";
+            break;
+          case 2:
+            baseOcean[i][j][0] = "miss";
+            break;
+          case 3:
+            baseOcean[i][j][0] = "destroyer2";
+            break;
+          case 4:
+            baseOcean[i][j][0] = "destroyed";
+            break;
           case 5:
-            this.ocean[i][j][0] = "water";
+            baseOcean[i][j][0] = "water";
+            break;
+          case 6:
+            baseOcean[i][j][0] = "patrolboat1H";
+            break;
+          case 7:
+            baseOcean[i][j][0] = "patrolboat2H";
+            break;
+          case 8:
+            baseOcean[i][j][0] = "patrolboat1";
+            break;
+          case 9:
+            baseOcean[i][j][0] = "patrolboat2";
+            break;
+          case 10:
+            baseOcean[i][j][0] = "submarine1H";
+            break;
+          case 11:
+            baseOcean[i][j][0] = "submarine2H";
+            break;
+          case 12:
+            baseOcean[i][j][0] = "submarine3H";
+            break;
+          case 13:
+            baseOcean[i][j][0] = "submarine1";
+            break;
+          case 14:
+            baseOcean[i][j][0] = "submarine2";
+            break;
+          case 15:
+            baseOcean[i][j][0] = "submarine3";
+            break;
+          case 16:
+            baseOcean[i][j][0] = "destroyer1H";
+            break;
+          case 17:
+            baseOcean[i][j][0] = "destroyer2H";
+            break;
+          case 18:
+            baseOcean[i][j][0] = "destroyer3H";
+            break;
+          case 19:
+            baseOcean[i][j][0] = "destroyer1";
+            break;
+          case 20:
+            baseOcean[i][j][0] = "destroyer2";
+            break;
+          case 21:
+            baseOcean[i][j][0] = "destroyer3";
+            break;
+          case 22:
+            baseOcean[i][j][0] = "battleship1H";
+            break;
+          case 23:
+            baseOcean[i][j][0] = "battleship2H";
+            break;
+          case 24:
+            baseOcean[i][j][0] = "battleship3H";
+            break;
+          case 25:
+            baseOcean[i][j][0] = "battleship4H";
+            break;
+          case 26:
+            baseOcean[i][j][0] = "battleship1";
+            break;
+          case 27:
+            baseOcean[i][j][0] = "battleship2";
+            break;
+          case 28:
+            baseOcean[i][j][0] = "battleship3";
+            break;
+          case 29:
+            baseOcean[i][j][0] = "battleship4";
+            break;
+          case 30:
+            baseOcean[i][j][0] = "aircraftcarrier1H";
+            break;
+          case 31:
+            baseOcean[i][j][0] = "aircraftcarrier2H";
+            break;
+          case 32:
+            baseOcean[i][j][0] = "aircraftcarrier3H";
+            break;
+          case 33:
+            baseOcean[i][j][0] = "aircraftcarrier4H";
+            break;
+          case 34:
+            baseOcean[i][j][0] = "aircraftcarrier5H";
+            break;
+          case 35:
+            baseOcean[i][j][0] = "aircraftcarrier1";
+            break;
+          case 36:
+            baseOcean[i][j][0] = "aircraftcarrier2";
+            break;
+          case 37:
+            baseOcean[i][j][0] = "aircraftcarrier3";
+            break;
+          case 38:
+            baseOcean[i][j][0] = "aircraftcarrier4";
+            break;
+          case 39:
+            baseOcean[i][j][0] = "aircraftcarrier5";
             break;
           default:
-            this.ocean[i][j][0] = "water";
+            baseOcean[i][j][0] = "water";
             break;
         }
       }
