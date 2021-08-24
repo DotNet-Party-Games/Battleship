505fe58132469c500d4dfe0ec7dbd19b7d342460
cayandle <cayandle@live.com>
2021-08-24 19:26:00 -0400

    Update rooms and shots

--- BattleshipServer/server.js
+++ BattleshipServer/server.js
@@ -6,33 +6,75 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 // create a collection of rooms
-const rooms = {};
+let rooms = [];
 // collection of messages
 const messages = [];
-
-const gameStart = new Map();
+const userMap = new Map();
 const teams = {};
+userMap.set("Lobby",{MaxPlayers:0,usersInRoom:[]});
 
 // logic for when a socket connects to the server
 // server event listener
 io.on('connection', socket => {
-    let previousRoomId = 5;
-    socket.join(previousRoomId);
+    let previousRoomId;
+    let username;
+
+    socket.on('first connection', user => {
+        username = user;
+        safeJoin("Lobby");
+    });
+
+    const RemoveUserFromList = () => {
+        if(userMap.get(previousRoomId)){
+            let temp = userMap.get(previousRoomId);
+            temp.usersInRoom=temp.usersInRoom.filter(user => user!=username);
+            userMap.set(previousRoomId, temp);
+            if(temp.usersInRoom.length==0 && previousRoomId != "Lobby"){
+                RemoveRoomFromList();
+            }
+        }
+    }
+
+    const RemoveRoomFromList = () => {
+        rooms = rooms.filter(place => place != previousRoomId);
+        io.emit('rooms', rooms);
+        console.log(previousRoomId + " removed from list");
+    }
+
+    const AddUserToList = () => {
+        if(userMap.get(previousRoomId)){
+            let temp = userMap.get(previousRoomId);
+            if(temp.usersInRoom.length>0){ 
+            temp.usersInRoom.push(username);
+            }
+            else{
+                temp.usersInRoom = [username];
+            }
+            userMap.set(previousRoomId,temp);
+            if(temp.MaxPlayers == temp.usersInRoom.length && previousRoomId != "Lobby"){
+                RemoveRoomFromList();
+            }
+        }
+    }
+
     // create a way to joins rooms
     const safeJoin = currentRoomId => {
         // leave previous room
-        //socket.leave(previousRoomId, () => console.log("left room"));
+        console.log(previousRoomId);
+        socket.leave(previousRoomId, () => console.log(`Socket ${socket.id} left room ${previousRoomId}`));
+        RemoveUserFromList();
         // join new room with debugging message to console
         socket.join(currentRoomId, () => console.log(`Socket ${socket.id} joined room ${currentRoomId}`));
         // keep track of current room
         previousRoomId = currentRoomId;
+        AddUserToList();
     };
 
     // logic for when a socket tries to join a room
     socket.on('join room', roomId => {
         safeJoin(roomId);
         // joining socket will know this one joined a room?
-        socket.emit('room', rooms[roomId]);
+        socket.emit('room', roomId);
     });
 
     socket.on("join team", otherplayer => {
@@ -41,10 +83,11 @@ io.on('connection', socket => {
 
     // logic for when a socket wants to add a room to the room list
     socket.on('add a room', room => {
-        rooms[room.id] = room;
+        rooms.push(room.id);
         safeJoin(room.id);
+        userMap.set(previousRoomId,{MaxPlayers:room.maxPlayers,usersInRoom:[]});
         // io.emit broadcasts to all clients, not just the acting socket
-        io.emit('rooms', Object.keys(rooms));
+        io.emit('rooms', rooms);
         socket.emit('room', room);
     });
 
@@ -59,13 +102,13 @@ io.on('connection', socket => {
 
 
     socket.on('send player board to opponent', (data)=> {
-        socket.broadcast.emit('enemy fleet', data);
+        socket.to(previousRoomId).emit('enemy fleet', data);
         console.log(data.ocean);
     });
 
     socket.on('send shot', (data)=>{
-        socket.broadcast.emit('enemy shoots',data);
-        socket.broadcast.emit('turn change', true)
+        socket.to(previousRoomId).emit('enemy shoots',data);
+        socket.to(previousRoomId).emit('turn change', true)
         socket.emit("turn change",false);
     });
 
@@ -93,7 +136,7 @@ io.on('connection', socket => {
         if(test==0){
             socket.emit('turn change', true);
         } else{
-            socket.broadcast.emit('turn change',true);
+            socket.to(previousRoomId).emit('turn change',true);
         }
 
 
@@ -102,24 +145,24 @@ io.on('connection', socket => {
     socket.on("send coordinates", (coords, room, userid)=>{
         console.log(coords);
     });
-    socket.on('Leave Room', roomnum =>{
-        console.log(`Socket ${socket.id} has left room ${roomnum}`);
-        socket.leave(roomnum);
+
+    socket.on('Leave Room', () =>{
+        safeJoin("Lobby");
     });
 
     socket.on('win shot', ()=>{
-        socket.broadcast.emit('loser', true);
+        socket.to(previousRoomId).emit('loser', true);
         socket.emit('winner', true);
     });
 
     // broadcast call rooms and sockets that have connected
-    io.emit('rooms', Object.keys(rooms));
+    io.emit('rooms', rooms);
 
     console.log(`Socket ${socket.id} has connected`);
 
     socket.on('disconnect', () =>{
-        socket.broadcast.emit('winner',true);
-        socket.leave(previousRoomId);
+        socket.to(previousRoomId).emit('winner',true);
+        safeJoin("");
     })
 });
 
