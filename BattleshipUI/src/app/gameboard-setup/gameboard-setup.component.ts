d635f6296b74a72f0a6663ddd3b2b37025b5f598
WingHLin <winghlin1998@gmail.com>
2021-08-25 12:19:17 -0600

    Rudimentary implementation of showing the air layer in gameboard-setup component

--- BattleshipUI/src/app/gameboard-setup/gameboard-setup.component.ts
+++ BattleshipUI/src/app/gameboard-setup/gameboard-setup.component.ts
@@ -6,6 +6,7 @@ import { BattleshipDeployService } from '../services/battleship-deploy.service';
 import { GameStateService } from '../services/gamestate.service';
 import { RoomService } from '../services/room.service';
 import { Ship } from '../services/ship';
+import { Orientation, Airplane } from '../services/airplane';
 
 @Component({
   selector: 'app-gameboard-setup',
@@ -17,7 +18,7 @@ export class GameboardSetupComponent implements OnInit {
   width: number[];
   height: number[];
   selected: number[] = new Array(2);  // holds the x,y coords for placing a ship (only tracking 1 endpoint)
-  test: string[][][] = new Array(10);  // array for placeholder space to test if ship can be placed
+  test: string[][][] = new Array(10);  // array for placeholder space to test if ship/airplane can be placed
   selectedShip: string;  // name of the ship selected to be placed
   isVertical: boolean = false;  // isRotated
   ships: Ship[] = new Array(5);
@@ -27,6 +28,11 @@ export class GameboardSetupComponent implements OnInit {
   shipsDeployed: boolean;
   roomId:string;
   opponentReady:boolean=false;
+  viewBoard: string = "air";  // tells the browser which gameboard to actively view; default should be "sea"
+  airplanes: Airplane[] = new Array(4);
+  airplanesDeployed: boolean;
+  airplaneOrientation: Orientation;
+  selectedAirplane: string;
 
   constructor(public auth: AuthService, private deploy:BattleshipDeployService, private router:Router, private roomservice:RoomService, private gamestate:GameStateService) {
     this.height = new Array(10);
@@ -37,20 +43,28 @@ export class GameboardSetupComponent implements OnInit {
       for(let j=0; j<10; j++){
         this.test[i][j]=new Array(2);
         this.test[i][j][0] = "water";
+        this.test[i][j][1] = "air";
       }
     }
     this.selected[0] = 0;
     this.selected[1] = 0;
-    this.selectedShip = "No Ship selected";
+    this.selectedShip = "";
+    this.selectedAirplane = "";
 
     for(let i =0; i < 5; i++){
       this.ships[i] = new Ship;
+    }
+
+    // this might be in an if?
+    for (let i = 0; i < 4; i++) {
+      this.airplanes[i] = new Airplane;
     }
 
     this.deploy.roomnum.subscribe(response=> this.roomNum=response);
     this.opponentId = "0";
     this.userId = "2";
     this.shipsDeployed = false;
+    this.airplanesDeployed = false;
     /*this.auth.idTokenClaims$.subscribe(
       (response) => {
         console.log(response);
@@ -96,14 +110,25 @@ export class GameboardSetupComponent implements OnInit {
     }
   } */
 
-  select(i:number, j:number){
+  selectWaterSpace(i:number, j:number){
     this.selected[0] = i;
     this.selected[1] = j;
     this.placeShip();
   }
 
+  selectAirSpace(i:number, j:number) {
+    this.selected[0] = i;
+    this.selected[1] = j;
+    this.placeAirplane();
+  }
+
   selectShip(s:string){
     this.selectedShip = s;
+  }
+
+  selectAirplane(s:string) {
+    this.selectedAirplane = s;
+    console.log(this.selectedAirplane + "has been selected!");
   }
 
   placeShip(){
@@ -123,7 +148,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "submarine":
@@ -141,7 +166,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "destroyer":
@@ -159,7 +184,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "battleship":
@@ -178,7 +203,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "aircraftcarrier":
@@ -198,7 +223,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         default:
@@ -221,7 +246,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "submarine":
@@ -239,7 +264,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "destroyer":
@@ -257,7 +282,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "battleship":
@@ -276,7 +301,7 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         case "aircraftcarrier":
@@ -296,13 +321,17 @@ export class GameboardSetupComponent implements OnInit {
           }
           else
           {
-            this.CannotPlaceError();
+            this.CannotPlaceError("sea");
           }
           break;
         default:
           break;
       }   
     }
+  }
+
+  placeAirplane() {
+
   }
 
   resetShip(ship:string){
@@ -333,13 +362,25 @@ export class GameboardSetupComponent implements OnInit {
     this.shipsDeployed = false;
   }
 
-  isplaced(ship:number){
+  resetAirplane(airplane:string) {
+
+  }
+
+  isShipPlaced(ship:number){
     if(this.ships[ship].placed){
       return true;
     }
     else{
       return false;
     }
+  }
+
+  isAirplanePlaced(airplane:number) {
+    if (this.airplanes[airplane].placed) {
+      return true;
+    }
+    
+    return false;
   }
 
   checkForSpace(size:number){
@@ -421,7 +462,12 @@ export class GameboardSetupComponent implements OnInit {
     this.router.navigate(["/game"]);
   }
 
-  CannotPlaceError(){
-    alert("Can't place {"+ this.selectedShip + "} at location {" + this.selected[0] + "," + this.selected[1]+ "}!");
+  CannotPlaceError(s:string){
+    if (s === "sea") {
+      alert("Can't place {"+ this.selectedShip + "} at location {" + this.selected[0] + "," + this.selected[1]+ "}!");
+    } else if (s === "air") {
+      alert("Can't place {"+ this.selectedAirplane + "} at location {" + this.selected[0] + "," + this.selected[1]+ "}!");
+    }
+    
   }
 }
