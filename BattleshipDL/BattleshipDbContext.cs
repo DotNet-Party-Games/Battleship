626312c42c2ca95667c2d06a15122ff544aae932
JSchla01 <penmaler@gmail.com>
2021-08-30 16:31:58 -0400

    Fix Database

--- BattleshipDL/BattleshipDbContext.cs
+++ BattleshipDL/BattleshipDbContext.cs
@@ -18,7 +18,7 @@ namespace BattleshipDL
 
         protected override void OnConfiguring(DbContextOptionsBuilder p_options)
         {
-            p_options.UseSqlServer(@"Server=tcp:revbox.database.windows.net,1433;Initial Catalog=BattleShipDB;Persist Security Info=False;User ID=revbox;Password=R3vb0xP@55;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //
         }
 
         protected override void OnModelCreating(ModelBuilder p_modelBuilder)
